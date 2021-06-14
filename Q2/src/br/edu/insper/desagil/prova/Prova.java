98d5ee5d17bb48e7068862fec28710d3bc3e9407
ricardo2909 <ricardo2909@users.noreply.github.com>
2021-06-14 12:08:07 -0300

    criando

---
+++ Q2/src/br/edu/insper/desagil/prova/Prova.java
@@ -0,0 +1,5 @@
+package br.edu.insper.desagil.prova;
+
+public class Prova {
+
+}
---
+++ Q2/test/br/edu/insper/desagil/prova/ProvaTest.java
@@ -0,0 +1,14 @@
+package br.edu.insper.desagil.prova;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProvaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
